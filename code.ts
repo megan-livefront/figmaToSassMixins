1fbf032cd4110c0d4ed5ac76262bb2bb5928d4f5
meganpekarek <megan@livefront.com>
2024-11-20 11:53:14 -0600

    consolidate mobile and desktop functions

--- code.ts
+++ code.ts
@@ -5,82 +5,61 @@
 
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === "figma") {
-  const textFrame = figma.currentPage.selection[0] as FrameNode;
-  const desktopFontItems = [];
-  const mobileFontItems = [];
-  textFrame.children.forEach((child) => {
-    if (child.type === "GROUP" && child.name === "Desktop Styles") {
-      child.children.forEach((fontSection) => {
-        if (fontSection.type === "FRAME" && fontSection.name === "StyleInfo") {
-          fontSection.children.forEach((fontInfoItem) => {
-            let fontName = "";
-            let fontSizeDesktop = "";
-            let lineHeightDesktop = "";
-            let letterSpacingDesktop = "";
-            if (fontInfoItem.type === "FRAME") {
-              fontInfoItem.children.forEach((details) => {
-                if (details.type === "FRAME" && details.children.length === 1) {
-                  fontName = (details.children[0] as TextNode).characters;
-                } else if (details.type === "FRAME") {
-                  details.children.forEach((fontData, index) => {
-                    if (index === 0)
-                      fontSizeDesktop = (fontData as TextNode).characters;
-                    else if (index === 2)
-                      lineHeightDesktop = (fontData as TextNode).characters;
-                    else if (index === 3)
-                      letterSpacingDesktop = (fontData as TextNode).characters;
-                  });
-                }
-              });
-              desktopFontItems.push({
-                fontName,
-                fontSizeDesktop,
-                lineHeightDesktop,
-                letterSpacingDesktop,
-              });
-            }
-          });
-        }
-      });
-      console.log("DESKTOP DATA", desktopFontItems);
-    } else if (child.type === "GROUP" && child.name === "Mobile Text Styles") {
-      child.children.forEach((fontSection) => {
-        if (fontSection.type === "FRAME" && fontSection.name === "StyleInfo") {
-          fontSection.children.forEach((fontInfoItem) => {
-            let fontName = "";
-            let fontSizeMobile = "";
-            let lineHeightMobile = "";
-            let letterSpacingMobile = "";
-            if (fontInfoItem.type === "FRAME") {
-              fontInfoItem.children.forEach((details) => {
-                if (details.type === "FRAME" && details.children.length === 1) {
-                  fontName = (details.children[0] as TextNode).characters;
-                } else if (details.type === "FRAME") {
-                  details.children.forEach((fontData, index) => {
-                    if (index === 0)
-                      fontSizeMobile = (fontData as TextNode).characters;
-                    else if (index === 2)
-                      lineHeightMobile = (fontData as TextNode).characters;
-                    else if (index === 3)
-                      letterSpacingMobile = (fontData as TextNode).characters;
-                  });
-                }
-              });
-              mobileFontItems.push({
-                fontName,
-                fontSizeMobile,
-                lineHeightMobile,
-                letterSpacingMobile,
-              });
-            }
-          });
-        }
-      });
-      console.log("MOBILE DATA", mobileFontItems);
-    }
-  });
+  const parentFrame = figma.currentPage.selection[0] as FrameNode;
+  const desktopFontItems = getFontData(parentFrame, "Desktop Styles");
+  const mobileFontItems = getFontData(parentFrame, "Mobile Text Styles");
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   // figma.closePlugin();
+}
+
+type FontData = {
+  fontName: string;
+  fontSize: string;
+  lineHeight: string;
+  letterSpacing: string;
+};
+
+function getFontData(parentFrame: FrameNode, nodeName: string) {
+  const fontData: FontData[] = [];
+  parentFrame.children.forEach((child) => {
+    if (child.type === "GROUP" && child.name === nodeName) {
+      child.children.forEach((fontSection) => {
+        if (fontSection.type === "FRAME" && fontSection.name === "StyleInfo") {
+          fontSection.children.forEach((fontInfoItem) => {
+            let fontName = "";
+            let fontSize = "";
+            let lineHeight = "";
+            let letterSpacing = "";
+            if (fontInfoItem.type === "FRAME") {
+              fontInfoItem.children.forEach((details) => {
+                if (details.type === "FRAME" && details.children.length === 1) {
+                  fontName = (details.children[0] as TextNode).characters;
+                } else if (details.type === "FRAME") {
+                  details.children.forEach((fontData, index) => {
+                    if (index === 0)
+                      fontSize = (fontData as TextNode).characters;
+                    else if (index === 2)
+                      lineHeight = (fontData as TextNode).characters;
+                    else if (index === 3)
+                      letterSpacing = (fontData as TextNode).characters;
+                  });
+                }
+              });
+              fontData.push({
+                fontName,
+                fontSize,
+                lineHeight,
+                letterSpacing,
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+
+  console.log(nodeName, fontData);
+  return fontData;
 }
