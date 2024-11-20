6a2751c7c9286738ad1f012769696bd99941a5b8
meganpekarek <megan@livefront.com>
2024-11-20 11:36:14 -0600

    cleanup

--- code.ts
+++ code.ts
@@ -5,12 +5,10 @@
 
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === "figma") {
-  // console.log("SELECTION", figma.currentPage.selection);
   const textFrame = figma.currentPage.selection[0] as FrameNode;
   const desktopFontItems = [];
   const mobileFontItems = [];
   textFrame.children.forEach((child) => {
-    // console.log(child);
     if (child.type === "GROUP" && child.name === "Desktop Styles") {
       child.children.forEach((fontSection) => {
         if (fontSection.type === "FRAME" && fontSection.name === "StyleInfo") {
@@ -81,17 +79,6 @@ if (figma.editorType === "figma") {
       console.log("MOBILE DATA", mobileFontItems);
     }
   });
-
-  // const nodes: SceneNode[] = [];
-  // for (let i = 0; i < numberOfRectangles; i++) {
-  //   const rect = figma.createRectangle();
-  //   rect.x = i * 150;
-  //   rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-  //   figma.currentPage.appendChild(rect);
-  //   nodes.push(rect);
-  // }
-  // figma.currentPage.selection = nodes;
-  // figma.viewport.scrollAndZoomIntoView(nodes);
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
